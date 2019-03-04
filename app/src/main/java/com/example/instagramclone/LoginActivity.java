fd842b61b4ed51128e0e198cc46ce29689e7092b
cmart1997 <christmartinez1997@yahoo.com>
2019-03-04 01:02:13 -0800

    Created Login Screen

---
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.instagramclone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    private static final String TAG = "LoginActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username,password);
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null)
+                {
+                    Log.e(TAG, "ISSUE WITH LOGIN");
+                    e.printStackTrace();
+                    return;
+                }
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+}
